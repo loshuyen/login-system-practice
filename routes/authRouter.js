b1a7a02b6efc5abd19154fbddeb94ec471ba4628
Shuyen Lo <sycw723@gmail.com>
2023-09-13 23:36:54 +0800

    add passportjs and session

---
+++ routes/authRouter.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const authRouter = express.Router();
+const db = require('../services/db');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+
+authRouter.get('/users/register', (req, res) => {
+  res.send('Registration Page!!!');
+});
+authRouter.get('/users/login', (req, res) => {
+  res.send('Login Page!!!');
+});
+
+//user registration
+authRouter.post('/users/register', async (req, res) => {
+  const { username, email } = req.body;
+  const password = await bcrypt.hash(req.body.password, 8);
+  const query = 'SELECT * FROM users WHERE username = ?';
+  const result = await db.promise().query(query, username);
+  const user = result[0][0];
+  if (!user) {
+    await db
+      .promise()
+      .query('INSERT INTO users SET ?', { username, password, email });
+    res.send(`username: ${username} registration success!`);
+    return;
+  }
+  res.send(`username: ${username} already exist!`);
+});
+
+authRouter.post(
+  '/users/login/local',
+  passport.authenticate('local', { failureRedirect: '/users/login' }),
+  (req, res) => {
+    res.redirect('/');
+  }
+);
+
+authRouter.get('/users/logout', (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      res.send(err);
+    }
+  });
+  res.redirect('/');
+});
+
+authRouter.get('/', (req, res) => {
+  res.send(req.user);
+});
+
+authRouter.get('/current_user', (req, res) => {
+  res.send(req.isAuthenticated());
+});
+
+module.exports = authRouter;
